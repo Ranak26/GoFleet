4538c50a3cd062269d966de661456db153bcfba5
Ranak <ranakpradhan55@gmail.com>
2026-04-12 16:35:04 +0530

    set up captains routes

--- Backend/app.js
+++ Backend/app.js
@@ -7,11 +7,16 @@ const connectDB = require('./db/db');
 connectDB();
 const userRoutes = require('./routes/user.routes');
 const cookieParser = require('cookie-parser');
+const captainRoutes = require('./routes/captain.routes');
+
+
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use('/captains', captainRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
